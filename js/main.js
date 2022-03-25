0015e06f3e2235921ff9d94117e988e64a0fc22e
TheRealHoko <julien.zeybel@live.fr>
2022-03-26 00:40:43 +0100

    wheel event

--- js/main.js
+++ js/main.js
@@ -4,7 +4,23 @@ import * as m from './mandelbrot.js';
 /**@type {HTMLCanvasElement} */
 
 var my_image = c.ctx.createImageData(800, 500);
+var f = {
+	zoomX : 0,
+	zoomY : 0,
+};
+c.canvas.addEventListener('wheel', e => {
+	if (e.wheelDelta > 0) {
+		f.zoomX = e.offsetX;
+		f.zoomY = e.offsetY;
+	} else {
+	}
+	console.log(f.zoomX, e.offsetY);
+	main();
+});
 
-console.log(my_image);
-m.renderFractal(my_image);
-c.ctx.putImageData(my_image, 0, 0);
+function main() {
+	m.renderFractal(my_image, f);
+	c.ctx.putImageData(my_image, 0, 0);
+}
+
+main();
--- js/mandelbrot.js
+++ js/mandelbrot.js
@@ -1,27 +1,31 @@
 import * as c from "./canvaslib.js";
 
-function mandelbrot_set(img, x, y) {
-	var xscale, yscale, i, zx, zy, xtemp;
+function mandelbrot_set(img, x, y, f) {
+	var	xscale, yscale, i, zx, zy, xtemp;
+	var	cx, cy;
 
 	xscale = x / img.width;
 	yscale = y / img.height;
 	zx = 0;
 	zy = 0;
+	cx = -1.6;
+	cy = -0.5;
 	i = 0;
-	while ((zx * zx + zy * zy) < 4 && i < 100) {
-		xtemp = zx*zx - zy*zy + xscale;
-		zy = 2 * zx * zy + yscale;
+	while ((zx * zx + zy * zy) < 4 && i < 30) {
+		xtemp = zx*zx - zy*zy + xscale + cx;;
+		zy = 2 * zx * zy + yscale + cy;
 		zx = xtemp;
 		i++;
 	}
 	return (i);
 }
 
-function renderFractal(img) {
+function renderFractal(img, f) {
 	var color = 0;
-	for (let x = 0; x < img.width; x++) {
-		for (let y = 0; y < img.height; y++) {
-			color = [mandelbrot_set(img, x, y), 0, 0, 255];
+	for (let x = f.zoomX; x < img.width; x++) {
+		for (let y = f.zoomY; y < img.height; y++) {
+			var rgb = mandelbrot_set(img, x, y, f);
+			color = [rgb*10, rgb, rgb*5, 255];
 			c.put_pixel(img, x, y, color);
 		}
 	}
